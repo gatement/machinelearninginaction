0aaa30b227fb1592433c59cd0895981c05c4550d
Johnson Lau <gatement@gmail.com>
2018-01-17 15:09:30 +0800

    go

---
+++ kNN.py
@@ -0,0 +1,48 @@
+from numpy import *
+import operator
+
+def createDataSet():
+  group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+  labels = ['A', 'A', 'B', 'B']
+  return group, labels
+
+def classify0(inX, dataSet, labels, k):
+  dataSetSize = dataSet.shape[0]
+  diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+  sqDiffMat = diffMat ** 2
+  sqDistances = sqDiffMat.sum(axis = 1)
+  distances = sqDistances ** 0.5
+  sortedDistIndices = distances.argsort()
+  classCount = {}
+  for i in range(k):
+    voteIlable = labels[sortedDistIndices[i]]
+    classCount[voteIlable] = classCount.get(voteIlable, 0) + 1
+  sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+  return sortedClassCount[0][0]
+
+def file2matrix(filename):
+  fr = open(filename)
+  arrayOlines = fr.readlines()
+  numberOfLines = len(arrayOlines)
+  returnMat = zeros((numberOfLines, 3))
+  classLabelVector = []
+
+  index = 0
+  for line in arrayOlines:
+    line = line.strip()
+    listFromLine = line.split('\t')
+    returnMat[index, :] = listFromLine[0:3]
+    classLabelVector.append(int(listFromLine[-1]))
+    index += 1
+
+  return returnMat, classLabelVector
+
+def autoNorm(dataSet):
+  minVals = dataSet.min(0)
+  maxVals = dataSet.max(0)
+  ranges = maxVals - minVals
+  normDataSet = zeros(shape(dataSet))
+  m = dataSet.shape[0]
+  normDataSet = dataSet - tile(minVals, (m, 1))
+  normDataSet = normDataSet/tile(ranges, (m, 1))
+  return normDataSet, ranges, minVals
---
+++ kNNPlot.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+from numpy import *
+from matplotlib import pyplot as plt
+import kNN
+
+datingDataMat, datingLables = kNN.file2matrix('datingTestSet2.txt')
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+ax.scatter(datingDataMat[:,0], datingDataMat[:, 1], 15.0 * array(datingLables), 15.0 * array(datingLables))
+
+plt.xlabel('flying miles')
+plt.ylabel('game time')
+
+plt.show()
